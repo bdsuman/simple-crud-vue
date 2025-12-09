9cc69b0931fcc24faefd384620d27347f6950b02
bdsuman <mesuman@yahoo.com>
2025-12-09 19:14:50 +0600

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,6 @@
 <?php
 
-use App\Models\Expert;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
-
-Route::get('/seed-experts', function () {
-    Artisan::call('db:seed', [
-        '--class' => 'ExpertSeeder',
-        '--force' => true,
-    ]);
-
-    return Expert::count();
-});
 
 Route::view('{any}', "welcome")->where('any', "^(?!api).*$");
 
